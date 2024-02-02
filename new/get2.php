2d8ba9ad99765cfcbf4e8b4b3c211ad8c60507da
Gabriel Achler <112950410+2rnu@users.noreply.github.com>
2024-02-02 16:36:36 +0100

    Add files via upload

---
+++ new/get2.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>
+   Wyniki
+  </title>
+ </head>
+
+ <?php 
+    echo $_GET['imie'];
+    echo "<br>";
+    echo $_GET['nazwisko'];
+ ?>
+ </body>
+</html>
---
+++ new/get3.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>
+   Wyniki
+  </title>
+ </head>
+
+ <?php 
+    echo $_POST['imie'];
+    echo "<br>";
+    echo $_POST['nazwisko'];
+ ?>
+ </body>
+</html>
